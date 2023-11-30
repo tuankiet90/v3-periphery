52c7edd75a48551798bbbeff1f1313d145dbb1bc
kmax3ever <kmax3ever2@gmail.com>
2023-11-30 23:53:00 +0700

    refactor script deploy

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -8,8 +8,9 @@ async function main() {
   const [owner] = await ethers.getSigners()
   var signer = owner
 
-  const v3Factory = '0x6F210f6079a2ef18c278B4d796B8Fd366b9fe08c' //test
-  const weth = '0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270' //test
+  const v3Factory = '0x6F210f6079a2ef18c278B4d796B8Fd366b9fe08c' 
+  const weth = '0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270' // WMATIC
+  const nFTPositionDescriptor=`0x91ae842A5Ffd8d12023116943e72A606179294f3` // use nFTPositionDescriptor of uniswap.
 
   const QuouterV2 = await ethers.getContractFactory('QuoterV2')
   const quouterV2 = await QuouterV2.deploy(v3Factory, weth)
@@ -18,30 +19,17 @@ async function main() {
   const tickLens = await TickLens.deploy()
   console.log('xxxx deploy TickLens :', tickLens.address)
 
-  const NFTDescriptorLib = await ethers.getContractFactory('NFTDescriptor')
-  const nFTDescriptorLib = await NFTDescriptorLib.deploy()
-
-  console.log('deploy nFTDescriptorLib ', nFTDescriptorLib.address)
-
-  const NFTPositionDescriptor = await ethers.getContractFactory('NonfungibleTokenPositionDescriptor', {
-    libraries: {
-      NFTDescriptor: `0xCAa8bCdd80e61EB5087116D2D7DD92ECdF3a6A7d`,
-    },
-  })
-  const nFTPositionDescriptor = await NFTPositionDescriptor.deploy(weth)
-  console.log('xxxx nFTPositionDescriptor :', nFTPositionDescriptor.address)
-
   const NonfungiblePositionManager = await ethers.getContractFactory('NonfungiblePositionManager')
   const nonfungiblePositionManager = await NonfungiblePositionManager.deploy(
     v3Factory,
     weth,
-    `0x5Ae46A6CF582dB11492DA02BbBd292a08B53315B`
+    nFTPositionDescriptor// use nFTPositionDescriptor of uniswap.
   )
 
   console.log('deploy nonfungiblePositionManager ', nonfungiblePositionManager.address)
 
   const V3Migrator = await ethers.getContractFactory('V3Migrator')
-  const v3Migrator = await V3Migrator.deploy(v3Factory, weth, `0x3aC184027C6bE4FEc47034362E8C87d170849cE2`)
+  const v3Migrator = await V3Migrator.deploy(v3Factory, weth, nonfungiblePositionManager.address)
 
   console.log('deploy V3Migrator ', v3Migrator.address)
 
@@ -61,12 +49,6 @@ async function main() {
     },
     {
       tickLens: tickLens.address,
-    },
-    {
-      nFTDescriptorLib: nFTDescriptorLib.address,
-    },
-    {
-      nFTPositionDescriptor: nFTPositionDescriptor.address,
     },
     {
       nonfungiblePositionManager: nonfungiblePositionManager.address,
